4d478e409c60336370f4726ed20eddb608a1570c
christophedacostasilva <christophedacostasilva@gmail.com>
2023-06-05 11:39:51 -0700

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,3 +1,11 @@
-for i in [0,1,2,3,4,5]
+for i in [0,1,2,3,4,5]:
   print(i)
+
+for z in range(6):
+  print(z) 
+
+
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+for name in names:
+  print(name)
   
